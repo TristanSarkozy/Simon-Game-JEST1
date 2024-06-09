1f8420e47fc452232228ae62d0d7b63fa6e63ac0
Suciu Dan <suciu_t_dan@yahoo.com>
2024-06-09 22:40:50 +0000

    Add new functions and test the functionality

--- 00_Boilerplate/scripts/tests/game.test.js
+++ 00_Boilerplate/scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 // Add Jest directive that runs before all tests are run
 // Add the node fs module, a file system handling module
@@ -34,5 +34,27 @@ describe("game object contains correct keys", () => {
     // Add  a fifth test with buttons as an array
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
